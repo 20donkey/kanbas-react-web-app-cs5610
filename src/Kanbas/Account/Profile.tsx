a6fb61a26d69969c7e76e1cd531e08f70a4fb4c5
NIAN LIU <77706155+20donkey@users.noreply.github.com>
2024-12-06 04:56:24 -0800

    Update Profile.tsx

--- src/Kanbas/Account/Profile.tsx
+++ src/Kanbas/Account/Profile.tsx
@@ -481,7 +481,7 @@ export default function Profile() {
   // };
   const updateProfile = async () => {
     const updatedProfile = await client.updateUser(profile);
-    dispatch(setCurrentUser(updatedProfile));
+    dispatch(setProfile(updatedProfile));
   };
 
 
