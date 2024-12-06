0520f9b2d4a71021725564048fdfca8303544473
NIAN LIU <77706155+20donkey@users.noreply.github.com>
2024-12-06 05:12:04 -0800

    Update Signin.tsx

--- src/Kanbas/Account/Signin.tsx
+++ src/Kanbas/Account/Signin.tsx
@@ -12,7 +12,7 @@ export default function Signin() {
     const user =  await client.signin(credentials);
     if (!user) return;
     dispatch(setCurrentUser(user));
-    navigate("/Kanbas/Dashboard");
+    navigate("/Kanbas/Account/Profile");
   };
 
   return (
